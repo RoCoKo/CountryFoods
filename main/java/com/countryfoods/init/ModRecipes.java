c205fbc97b70e3299d9127636ec488cfc8796177
RoCoKo <rroccokko@gmail.com>
2019-07-30 18:22:39 +0300

    Update ModRecipes.java

--- main/java/com/countryfoods/init/ModRecipes.java
+++ main/java/com/countryfoods/init/ModRecipes.java
@@ -2,12 +2,14 @@ package com.countryfoods.init;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import com.countryfoods.init.ModBlocks;
+import com.countryfoods.init.ModItems;
 
 public class ModRecipes 
 {
-
 	public static void init() 
 	{
 		GameRegistry.addSmelting(ModItems.RAW_KEBAB, new ItemStack(ModItems.KEBAB, 1), 1.5f);
+		GameRegistry.addSmelting(ModItems.RAW_CHEESECAKE, new ItemStack(ModBlocks.CHEESECAKE, 1), 1.5f);
 	}
 }
